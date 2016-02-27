c8a56f47a5be4aa6295df82c091c84f20c43876f
kukunoorusvadrut <kukunoorusvadrut@outlook.com>
2016-02-26 17:52:29 -0800

    Create Typing_Picker.py

---
+++ Typing_Picker.py
@@ -0,0 +1,14 @@
+# 10fastfingers.com
+# typing.lk
+# typing.com/games
+# typingclub.com
+# import the magic random picker
+
+# list of things to pick from
+typing_options = ['abc', 'bcd', 'cde']
+
+# find a way to pick something
+typing_choice = typing_options[2]
+
+# a way to tell the user what to do
+print typing_choice
