866f69abc8854b7623d66aeb49f2b9eab9f09a4f
kukunoorusvadrut <kukunoorusvadrut@outlook.com>
2016-03-11 17:09:49 -0800

    Update Typing_Picker.py

--- Typing_Picker.py
+++ Typing_Picker.py
@@ -5,10 +5,10 @@
 # import the magic random picker
 
 # list of things to pick from
-typing_options = ['abc', 'bcd', 'cde']
+typing_options = ['abc', 'def', 'ghi']
 
 # find a way to pick something
-typing_choice = typing_options[2]
+typing_choice = typing_options[1]
 
 # a way to tell the user what to do
 print typing_choice
